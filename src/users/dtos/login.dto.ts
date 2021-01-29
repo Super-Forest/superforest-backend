e736c1771b2da80fa891c1d107af9a9e7ae7228e
dan-supetree <danyj@supertree.co>
2021-01-29 15:11:03 +0900

    feat: add login dto and login to userService

---
+++ src/users/dtos/login.dto.ts
@@ -0,0 +1,12 @@
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutPut } from 'src/common/dtos/output.dto';
+import { User } from '../entities/user.entity';
+
+@InputType()
+export class LoginInPut extends PickType(User, ['email', 'password']) {}
+
+@ObjectType()
+export class LoginOutPut extends CoreOutPut {
+  @Field(() => String, { nullable: true })
+  token: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInPut } from './dtos/createAccount.dto';
+import { LoginInPut } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -30,6 +31,38 @@ export class UsersService {
       return {
         ok: false,
         error: 'Could not create account',
+      };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInPut): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+
+      if (!user.checkPassword(password)) {
+        return {
+          ok: false,
+          error: 'wrong password',
+        };
+      }
+
+      return {
+        ok: true,
+        token: '',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'Could not login',
       };
     }
   }
